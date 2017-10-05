b397d1b99e5aaf53ebcf0bfaf1b29eaa885fd7ed
Polly <polina_simonova_95@mail.ru>
2017-10-05 14:02:34 +0400

    Create common.js

---
+++ common.js
@@ -0,0 +1,16 @@
+var f =document.getElementById('Foo');
+
+var i = 0;
+
+setInterval(function (){
+    f.style.display = (f.style.display == 'none' ? '' : 'none');
+    i++;
+    if (i % 2 === 0){
+        if (f.innerHTML.indexOf("LOVE") !=-1){
+            s = 'SKIING';
+        } else {
+            s = 'LOVE';
+        }
+        f.innerHTML = s;
+    }
+},750);
